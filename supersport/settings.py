96354bd7d85dc2a4c78cff252d1ea5d482b678ce
ErrQD0303 <quocdat3396@gmail.com>
2024-04-03 15:53:34 +0700

    Update git .ignore

--- supersport/settings.py
+++ supersport/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "supersportdb",
         "USER": "root",
-        "PASSWORD": "Ngocquach97",
+        "PASSWORD": "sh1n1ch1",
         "HOST": "localhost",
         "PORT": "3306",
     }

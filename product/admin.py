5e76b24c29bc51ca9825899b17762ba6bd1a3c3d
Tien Phan <phanvantien.sgn@gmai.com>
2024-04-12 22:19:02 +0700

    commit slug

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Category, Product, ProductDetail
+from .models import Category, Product, ProductDetail, Brand
+
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
+
+class BrandAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('brand_name',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(ProductDetail)
--- product/models.py
+++ product/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
 class Brand(models.Model):
     brand_name = models.CharField(max_length=250, db_index=True)
     slug = models.SlugField(max_length=250, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.brand_name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.brand_name
@@ -15,6 +20,10 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.category_name
@@ -33,10 +42,17 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.product_name)
+        super().save(*args, **kwargs)
+
 class ProductDetail(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     size = models.CharField(max_length=20, null=True)
     quantity = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'ProductDetails'
 
     def __str__(self):
         return f'{self.product.__str__()} - {self.quantity}'

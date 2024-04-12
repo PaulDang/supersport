ebdd7bcf0ab9649de91a3b89d0b92b3b43105cfa
Tien Phan <phanvantien.sgn@gmai.com>
2024-04-12 21:07:51 +0700

    commit product model

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'product',
+    'cart',
+    'order',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -135,6 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = BASE_DIR / "static/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- product/models.py
+++ product/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.brand_name
+
+class Categgory(models.Model):
+    categgory_name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.categgory_name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250, db_index=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Categgory, on_delete=models.CASCADE, null=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=0)
+    image = models.ImageField(upload_to='images/', blank=True)
+    total_quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.product_name
+
+class ProductDetail(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    size = models.CharField(max_length=20, null=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.product.__str__()} - {self.quantity}'
+

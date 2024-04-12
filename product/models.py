06a84626bc85eb3653bd7e10001493126e055aef
Tien Phan <phanvantien.sgn@gmai.com>
2024-04-12 21:42:18 +0700

    commit admin for product

--- product/models.py
+++ product/models.py
@@ -9,20 +9,20 @@ class Brand(models.Model):
     def __str__(self):
         return self.brand_name
 
-class Categgory(models.Model):
-    categgory_name = models.CharField(max_length=250, db_index=True)
+class Category(models.Model):
+    category_name = models.CharField(max_length=250, db_index=True)
     slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return self.categgory_name
+        return self.category_name
 
 class Product(models.Model):
     product_name = models.CharField(max_length=250, db_index=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
-    category = models.ForeignKey(Categgory, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=250, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
@@ -40,4 +40,3 @@ class ProductDetail(models.Model):
 
     def __str__(self):
         return f'{self.product.__str__()} - {self.quantity}'
-

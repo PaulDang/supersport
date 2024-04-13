94283613eb8e91127cb9b712a88058c453304d2d
Tien Phan <phanvantien.sgn@gmai.com>
2024-04-13 10:55:30 +0700

    fix product

--- product/admin.py
+++ product/admin.py
@@ -12,18 +12,6 @@ class ProductAdmin(admin.ModelAdmin):
         models.ImageField: {'widget': AdminThumbnail(image_field='thumbnail')}
     }
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'images':
-            kwargs['widget'] = admin.MultipleImageWidget
-            return admin.ModelAdmin.formfield_for_dbfield(self, db_field, **kwargs)
-        return super(ProductAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-
-    def save_form(self, request, form, instance):
-        form.save()
-        if request.FILES.get('images'):
-            for image_file in request.FILES.getlist('images'):
-                product_image = ProductImage.objects.create(image=image_file, product=instance)
-
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('brand_name',)}

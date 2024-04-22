556460a9e0a57e186a6b18629cea5f5fedd8a302
Bono <bono43@nexondv.com>
2024-04-22 13:30:44 +0700

    refactor: config user-info UI and redirect them suitably

--- order/views.py
+++ order/views.py
@@ -10,56 +10,50 @@ from django.urls import resolve
 
 def generate_product_details_object(product_detail: ProductDetail):
     return {
-        'id': product_detail.pk,
-        'product_name': product_detail.product.product_name,
-        'image_url': product_detail.product.images.all()[0].image,
-        'price': product_detail.product.price,
-        'quantity': product_detail.quantity
+        "id": product_detail.pk,
+        "product_name": product_detail.product.product_name,
+        "image_url": product_detail.product.images.all()[0].image,
+        "price": product_detail.product.price,
+        "quantity": product_detail.quantity,
     }
 
 
 def generate_data(request):
-    product_details = ProductDetail.objects.filter(
-        cartdetail__cart__user=request.user)
+    product_details = ProductDetail.objects.filter(cartdetail__cart__user=request.user)
     return list(map(generate_product_details_object, product_details))
 
 
-@login_required(login_url='signin')
+@login_required(login_url="signin")
 def checkout(request):
     if request.method == "POST":
         data = generate_data(request)
         total_price = 0
         for item in data:
-            item_total = item['price'] * item['quantity']
-            item['item_total'] = item_total
+            item_total = item["price"] * item["quantity"]
+            item["item_total"] = item_total
             total_price = total_price + item_total
 
-        template = 'checkout.html'
-        context = {
-            'data': data,
-            'total_price': total_price
-        }
-        return render(request=request,
-                      template_name=template,
-                      context=context)
+        template = "checkout.html"
+        context = {"data": data, "total_price": total_price}
+        return render(request=request, template_name=template, context=context)
 
 
-@login_required(login_url='signin')
+@login_required(login_url="signin")
 def placeorder(request):
     data = generate_data(request)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CheckoutForm(request.POST)
 
         if form.is_valid():
             order = form.save(commit=False)
             order.user = request.user
-            order.payment_mode = request.POST.get('payment_mode')
+            order.payment_mode = request.POST.get("payment_mode")
 
             total_price = 0
             for item in data:
-                item_total = item['price'] * item['quantity']
-                item['item_total'] = item_total
+                item_total = item["price"] * item["quantity"]
+                item["item_total"] = item_total
                 total_price = total_price + item_total
 
             order.total_price = total_price
@@ -73,23 +67,22 @@ def placeorder(request):
             #         quantity=item['quantity']
             #     )
 
-            return redirect('/order-summary')
+            return redirect("/order-summary")
 
         else:
-            return render(request, "checkout.html", {'form': form})
+            return render(request, "checkout.html", {"form": form})
     else:
         form = CheckoutForm(None)
-        return render(request, 'checkout.html', {'form': form})
+        return render(request, "checkout.html", {"form": form})
 
 
 def ordersummary(request):
-    template = loader.get_template('ordersummary.html')
-    return HttpResponse(template.render())
+    return render(request, "ordersummary.html")
+
 
 def aboutus(request):
-    template = loader.get_template('aboutus.html')
-    return HttpResponse(template.render())
+    return render(request, "aboutus.html")
+
 
 def contactus(request):
-    template = loader.get_template('contactus.html')
-    return HttpResponse(template.render())
+    return render(request, "contactus.html")

a77164bbbbf5cf2edf8a9cdc49f59932909282e0
Tien Phan <phanvantien.sgn@gmai.com>
2024-04-24 21:04:47 +0700

    refactor model admin of package product

--- product/admin.py
+++ product/admin.py
@@ -3,38 +3,34 @@ from imagekit.admin import AdminThumbnail
 from django.db import models
 from .models import Category, Product, ProductDetail, Brand, ProductImage
 
+class ImageInline(admin.StackedInline):
+    model = ProductImage
 
+class ProductDetailInline(admin.StackedInline):
+    model = ProductDetail
+    extra = 1  # Number of empty inline forms to display
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ImageInline, ProductDetailInline]
     formfield_overrides = {
         models.ImageField: {'widget': AdminThumbnail(image_field='thumbnail')}
     }
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'images':
-            kwargs['widget'] = admin.MultipleImageWidget
-            return admin.ModelAdmin.formfield_for_dbfield(self, db_field, **kwargs)
-        return super(ProductAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-
-    # def save_form(self, request, form, instance):
-    #     form.save()
-    #     if request.FILES.get('images'):
-    #         for image_file in request.FILES.getlist('images'):
-    #             product_image = ProductImage.objects.create(
-    #                 image=image_file, product=instance)
-
+    def get_form(self, request, obj=None, **kwargs):
+        FormClass = super().get_form(request, obj, **kwargs)
+        FormClass.base_fields['total_quantity'].disabled = True
+        return FormClass
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('brand_name',)}
-
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('category_name',)}
 
 
-admin.site.register(ProductDetail)
 admin.site.register(ProductImage)
+admin.site.register(ProductDetail)

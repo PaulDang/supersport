9e3ad475b1c0498b1548cdf7bb320b68dc9b506a
ErrQD0303 <quocdat3396@gmail.com>
2024-04-27 09:21:10 +0700

    temporary

--- product/models.py
+++ product/models.py
@@ -18,6 +18,7 @@ class Brand(models.Model):
     def __str__(self):
         return self.brand_name
 
+
 class Category(models.Model):
     category_name = models.CharField(max_length=250, db_index=True)
     slug = models.SlugField(max_length=250, unique=True)
@@ -35,20 +36,25 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('list-category', args=[self.slug])
 
+
 class Product(models.Model):
     product_name = models.CharField(max_length=250, db_index=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
-    categories = models.ManyToManyField(Category,blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=250, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
     discount_price = models.DecimalField(max_digits=10, decimal_places=0)
     total_quantity = models.IntegerField(default=0)
+    # anh Tiến kiểm tra dùm em line này xem có work không
+    sizes = models.JSONField(default={})
 
     def __str__(self):
         return self.product_name
 
     def save(self, *args, **kwargs):
+        if not self.sizes:
+            self.sizes = {}
         self.slug = slugify(self.product_name)
         super().save(*args, **kwargs)
 
@@ -60,7 +66,8 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):

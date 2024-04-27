8cc8b177682090c6ca6bef21dff865357e1dda50
ErrQD0303 <quocdat3396@gmail.com>
2024-04-27 09:21:10 +0700

    temporary

--- product/models.py
+++ product/models.py
@@ -46,11 +46,15 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=0)
     discount_price = models.DecimalField(max_digits=10, decimal_places=0)
     total_quantity = models.IntegerField(default=0)
+    # anh Tiến kiểm tra dùm em line này xem có work không
+    sizes = models.JSONField(default={})
 
     def __str__(self):
         return self.product_name
 
     def save(self, *args, **kwargs):
+        if not self.sizes:
+            self.sizes = {}
         self.slug = slugify(self.product_name)
         super().save(*args, **kwargs)
 

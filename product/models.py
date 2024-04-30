b3ad086d4039d4c97679e4a19a0630f32bf3b57e
Paul Dang <dangvanthanhdli@gmail.com>
2024-04-30 16:48:18 +0700

    add categories on main page

--- product/models.py
+++ product/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
 from django.utils.safestring import mark_safe
 from django.urls import reverse
 
-
-# Create your models here.
 class Brand(models.Model):
     brand_name = models.CharField(max_length=250, db_index=True)
     slug = models.SlugField(max_length=250, unique=True)
@@ -17,7 +13,6 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.brand_name
-
 
 class Category(models.Model):
     category_name = models.CharField(max_length=250, db_index=True)
@@ -36,16 +31,18 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('list-category', args=[self.slug])
 
-
 class Product(models.Model):
     product_name = models.CharField(max_length=250, db_index=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
-    categories = models.ManyToManyField(Category, blank=True)
+    categories = models.ManyToManyField(Category, related_name='products', blank=True)
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=250, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
     discount_price = models.DecimalField(max_digits=10, decimal_places=0)
     total_quantity = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return self.product_name
@@ -60,13 +57,9 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product-info', args=[self.slug])
 
-    def __generate_sizes_dict(self, data: str):
-        data
-
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
@@ -85,7 +78,6 @@ class ProductDetail(models.Model):
         return f'{self.product.__str__()} - {self.quantity}'
 
     def save(self, *args, **kwargs):
-        # Update total_quantity of the product when saving product detail
         if self.pk is None:  # Create new instance
             super().save(*args, **kwargs)
             self.product.total_quantity += self.quantity
@@ -98,7 +90,6 @@ class ProductDetail(models.Model):
             super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        # Update total_quantity of the product when deleting product detail
         self.product.total_quantity -= self.quantity
         self.product.save()
         super().delete(*args, **kwargs)

0fbbf937a750e1c103e6b31804350ff1078f0f95
Paul Dang <dangvanthanhdli@gmail.com>
2024-05-23 10:35:54 +0700

    add url for new arrival

--- product/urls.py
+++ product/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product/<slug:slug>/', views.product_info, name='product-info'),
     path('category/<slug:slug>/', views.list_categories,name='list-category'),
     path('search/', views.search_product, name='search_product'),
+    path('products/new-arrival', views.new_arrival, name='new-arrival')
 ]

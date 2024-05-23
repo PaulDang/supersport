0fb5be712ec3ab38b70e686201a54edb9b4df891
Paul Dang <dangvanthanhdli@gmail.com>
2024-05-23 10:34:54 +0700

    added new arrival  function

--- product/views.py
+++ product/views.py
@@ -5,9 +5,7 @@ from .models import Category, Product, Brand, ProductImage, ProductDetail
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.db.models import Sum
-
-# Create your views here.
-
+from django.http import JsonResponse
 
 class ProductManager:
     def __init__(self):
@@ -19,13 +17,14 @@ class ProductManager:
             return Product.objects.filter(categories=category)
         return []
 
-    def paginate_products(self, products, page_number):
-        paginator = Paginator(products, self.items_per_page)
+    def paginate_products(self, products, page_number, per_page=None):
+        if per_page is not None:
+            paginator = Paginator(products, per_page)
+        else:
+            paginator = Paginator(products, self.items_per_page)
         return paginator.get_page(page_number)
 
     def get_context(self, request):
-        all_products = Product.objects.all()
-
         nam_products = self.get_category_products('nam')
         nu_products = self.get_category_products('nu')
         giay_products = self.get_category_products('giay')
@@ -37,21 +36,31 @@ class ProductManager:
         giay_page_obj = self.paginate_products(giay_products, page_number)
 
         return {
-            'all_products': all_products,
             'nam_page_obj': nam_page_obj,
             'nu_page_obj': nu_page_obj,
             'giay_page_obj': giay_page_obj,
         }
 
+    def new_arrival(self, request):
+        all_products = Product.objects.all().order_by('-id')
+        page_number = request.GET.get('page', 1)
+        all_products_obj = self.paginate_products(all_products, page_number, per_page=12)
+        context = {
+            'all_products_obj': all_products_obj,
+        }
+        return render(request, 'product/new-arrival.html', context)
 
 def store(request):
     product_manager = ProductManager()
     context = product_manager.get_context(request)
     return render(request, 'product/store.html', context)
 
+def new_arrival(request):
+    product_manager = ProductManager()
+    return product_manager.new_arrival(request)
+
 def search_product(request):
     query = request.GET.get('q')
-    print("Query:", query)  # In ra giá trị của query
     if query:
         products = Product.objects.filter(product_name__icontains=query)
         data = []
@@ -65,7 +74,6 @@ def search_product(request):
                 'images': [image.image.url for image in product.images.all()]
             }
             data.append(product_data)
-        print("Results:", data)
         return JsonResponse(data, safe=False)
     else:
         return JsonResponse([], safe=False)
@@ -73,7 +81,6 @@ def search_product(request):
 def categories(request):
     all_categories = Category.objects.all()
     return {'all_categories': all_categories}
-
 
 def product_info(request, slug):
     product = get_object_or_404(Product, slug=slug)
@@ -91,7 +98,6 @@ def product_info(request, slug):
         'sizes': sizes
     }
     return render(request, 'product/product-info.html', context)
-
 
 def list_categories(request, slug=None):
     category = get_object_or_404(Category, slug=slug)
